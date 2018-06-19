bab4359d382db931c1e1480320a1685166a9e932
mauro-mustillo <mmustillo@shift3tech.com>
2018-06-18 18:48:54 -0700

    eventlistener

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+console.log();
+let emailInput = document.forms.addItem.addToDo;
+let myPTag = document.querySelector('#data_binder');
+emailInput.addEventListener('keyup', e => {
+	console.log(e.keyCode);
+});
