358572049228693ef44cf1f0ad33b132854c8254
mauro-mustillo <mmustillo@shift3tech.com>
2018-06-20 16:53:54 -0700

    Added More stuff 6/20

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,14 +7,12 @@ document.querySelector(".add-to-list").addEventListener("click", function () {
 	} else {
 
 		let listItem = document.createElement("LI"); // Create LI
-		let checkbox = document.createElement('input'); // Create INPUT
+		listItem.appendChild(document.createTextNode(grabListItem)); // Grab text from INPUT to LABEL
+		listItem.className = "unchecked";
+	/*	let checkbox = document.createElement('input'); // Create INPUT
 		checkbox.type = "checkbox"; // set Checkbox property
 		checkbox.name = "name"; // set Name property
 		checkbox.value = "value"; // set Value property
-
-	
-		
-		
 		checkbox.id = "id" ; // set ID property
 
 		let label = document.createElement('label'); // Create LABEL
@@ -23,7 +21,7 @@ document.querySelector(".add-to-list").addEventListener("click", function () {
 
 		listItem.appendChild(checkbox); // Add Checkbox to li
 		listItem.appendChild(label); // Add LABEL to li	 	
-
+*/
 		let edit = document.createElement('div'); // Create DIV for EDIT AREA
 		edit.className = "edit-control"; // Set CLASS for EDIT AREA div
 		edit.innerHTML = '<button class="edit"><span class="icon-edit"></span></button><button class="delete"><span class="icon-remove"></span></button>'; // Code for EDIT AREA
@@ -32,3 +30,16 @@ document.querySelector(".add-to-list").addEventListener("click", function () {
 
 	}
 });
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+
+
+
