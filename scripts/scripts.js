4f94b41adfaedf998a46934c25290f6638107956
mauro-mustillo <mmustillo@shift3tech.com>
2018-06-20 23:48:06 -0700

    Added Create Array Capacity

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,35 +2,28 @@
 window.addEventListener('keydown',function(e){if(e.keyIdentifier==='U+000A'||e.keyIdentifier==='Enter'||e.keyCode===13){if(e.target.nodeName==='INPUT'&&e.target.type==='text'){e.preventDefault();return false;}}},true); // prevent ENTER key to submit form
 
 //grab text from input and Create LI
+
+let CreatedToDos = []; //create array from Submissions
+
+
+
 document.querySelector(".add-to-list").addEventListener("click", function () {
-	const grabListItem = document.querySelector(".nice-day").value;
+	const grabListItem = document.forms.addItems.addToDo.value;
 	if (grabListItem === '') {
 		alert('Please add something to list'); // Alert if nothing is TYPED
 	} else {
-
+CreatedToDos.push(grabListItem);
 		let listItem = document.createElement("LI"); // Create LI
 		listItem.appendChild(document.createTextNode(grabListItem)); // Grab text from INPUT to LABEL
 		listItem.className = "unchecked";
 		listItem.onclick = "closebtns" ;
-	/*	let checkbox = document.createElement('input'); // Create INPUT
-		checkbox.type = "checkbox"; // set Checkbox property
-		checkbox.name = "name"; // set Name property
-		checkbox.value = "value"; // set Value property
-		checkbox.id = "id" ; // set ID property
-
-		let label = document.createElement('label'); // Create LABEL
-		label.htmlFor = "id";
-		label.appendChild(document.createTextNode(grabListItem)); // Grab text from INPUT to LABEL
-
-		listItem.appendChild(checkbox); // Add Checkbox to li
-		listItem.appendChild(label); // Add LABEL to li	 	
 
 		let edit = document.createElement('div'); // Create DIV for EDIT AREA
 		edit.className = "edit-control"; // Set CLASS for EDIT AREA div
 		edit.innerHTML = '<button class="edit"><span class="icon-edit"></span></button><button class="delete" onclick="removeMe()"><span class="icon-remove"></span></button>'; // Code for EDIT AREA
 		listItem.appendChild(edit); //Add EDIT AREA to li 	
 		document.querySelector(".list").appendChild(listItem); //Add li to UL
-
+console.log(CreatedToDos);
 	}
 });
 
